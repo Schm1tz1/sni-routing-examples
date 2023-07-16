156f2b1579651b300476febaba8c2b8300bcef92
Roman Schmitz <Schm1tz1@users.noreply.github.com>
2023-07-16 13:24:16 +0200

    Update create-certs.sh

--- nginx-tls-termination/create-certs.sh
+++ nginx-tls-termination/create-certs.sh
@@ -35,6 +35,6 @@ docker run --rm \
 -e PASSWD=changeIt -e DAYS=389 -e DAYS_CA=3650 \
 -v $(pwd)/hosts.yaml:/opt/certs/hosts.txt \
 -v $(pwd)/certs:/opt/certs/current \
-schmitzi/openssl-alpine-j11:3.1.7
+schmitzi/openssl-alpine-j11:1.0.0
 
 rm ./hosts.yaml
